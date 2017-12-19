ef9b9940a72759d2adad62a66691fe0db9db8c04
Uongjo <Uongjo@utexas.edu>
2017-12-19 09:27:42 -0600

    Updated valid setup check method

--- src/Board.java
+++ src/Board.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class Board {
 	//2D Array as internal storage container
 	int[][] puzzle;
-	
+
 	/**
 	 * Default Constructor
 	 * </br>pre: None
@@ -19,9 +19,9 @@ public class Board {
 	public Board() {
 		puzzle = new int[9][9];
 	}
-	
+
 	/**
-	 * Constructor takes in 2D array and creates a deep copy
+	 * Constructor takes in 2D array and creates a deep copy of n to puzzle
 	 * </br>pre: n must be a 9 x 9 matrix. All values must be between 0 - 9 inclusive, where 0 is an empty spot
 	 * </br>post: Instantiation of puzzle 
 	 * @param n - 2D array of ints
@@ -39,11 +39,11 @@ public class Board {
 			}
 		}
 	}
-	
+
 	/**
 	 * Set Method for puzzle - sets a specified position to a value
 	 * </br>pre: 0 <= row < 9, 0 <= col < 9, 1 <= val <= 9
-	 * </br>post: puzzle is altered by result of method call
+	 * </br>post: puzzle[row][col] = val
 	 * @param row - row based on 0 base indexing
 	 * @param col - column based on 0 base indexing
 	 * @param val - value that should be inserted at position(row, col)
@@ -53,11 +53,10 @@ public class Board {
 			throw new IllegalArgumentException("Parameters out of bounds");
 		puzzle[row][col] = val;
 	}
-	
+
 	/**
 	 * Get Method for puzzle - gets a value given position
 	 *</br> pre: 0 <= row < 9, 0 <= col < 9
-	 *</br> post: none
 	 * @param row - row based on 0 base indexing 
 	 * @param col - column based on 0 base indexing
 	 * @return Value specified at position(row, col)
@@ -71,16 +70,58 @@ public class Board {
 	/**
 	 * Checks and see if the current Sudoku puzzle is a valid set up (excluding 0's)
 	 * </br>pre: none
-	 * </br>post: none
 	 * @return Returns true if the current Sudoku puzzle is a valid set up, false otherwise
 	 */
 	public boolean isValidSetup() {
-		for(int row = 0; row < puzzle.length; row++) {
-			
+		if(!rowCheck())
+			return false;
+		if(!columnCheck())
+			return false;
+		if(!subGridCheck())
+			return false;
+		return true;
+	}
+
+	/**
+	 * Helper method for isValidSetup - Checks rows for non-zero duplicates
+	 * </br>pre: none
+	 * @return Returns true if all rows contain no duplicates, false otherwise
+	 */
+	private boolean rowCheck() {
+		for(int[] row : puzzle) {
+			for(int i = 0; i < row.length; i++) {
+				if(row[i] != 0) {
+					for(int j = i + 1; j < row.length; j++) {
+						if(row[i] == row[j])
+							return false;
+					}
+				}
+			}
 		}
 		return true;
 	}
-	
+
+	/**
+	 * Helper method for isValidSetup - Checks columns for non-zero duplicates
+	 * </br>pre: none
+	 * @return Returns true if all columns contain no duplicates, false otherwise
+	 */
+	private boolean columnCheck() {
+		for(int col = 0; col < puzzle[0].length; col++) {
+
+		}
+		return true;
+	}
+
+	/**
+	 * Helper method for isValidSetup - Checks sub divisions for non-zero duplicates
+	 * </br>pre: none
+	 * @return Returns true if all sub divisions contain no duplicates, false otherwise
+	 */
+	private boolean subGridCheck() {
+		return true;
+	}
+
 	/**
 	 * Method prints out the Sudoku board in a string representation
 	 * </br>pre: none
