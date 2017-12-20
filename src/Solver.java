d4dd3a6407365fa1111f02c3791e1882be0cec01
Uongjo <Uongjo@utexas.edu>
2017-12-20 01:15:47 -0600

    Main solver method completed

--- src/Solver.java
+++ src/Solver.java
@@ -45,4 +45,48 @@ public class Solver {
 		}
 		board = new Board(con);
 	}
+	
+	/**
+	 * Kick off method for helpSolve method
+	 * </br>pre: None
+	 * @return If Puzzle can be solved, returns solved board in String format, 
+	 * otherwise returns error message and the original board
+	 */
+	public String solve() {
+		String originalBoard = board.toString();
+		if(!helpSolve(0, 0))
+			return "***PUZZLE COULD NOT BE SOLVED***\n" + originalBoard;
+		else
+			return board.toString();
+	}
+	
+	/**
+	 * Uses recursive backtracking to solve Sudoku puzzle
+	 * </br>pre: none
+	 * </br>post: board is altered as result of method call
+	 * @param row - row based on 0 base indexing
+	 * @param col - column based on 0 base indexing
+	 * @return returns true if Sudoku puzzle is solveable, false otherwise
+	 */
+	private boolean helpSolve(int row, int col) {
+		System.out.println(board);
+		if(board.isValidSetup()) {
+			if(row == board.getDimension() - 1 && col == board.getDimension()) {
+				return true;
+			}else if(col == board.getDimension()) {
+				col = 0;
+				row += 1;
+			}
+			for(int val = 1; val <= board.getDimension(); val++) {
+				if(board.getVal(row, col) == 0) {
+					board.setVal(row, col, val);
+				}
+				if(helpSolve(row, col + 1)) {
+					return true;
+				}
+				board.setVal(row, col, 0);
+			}
+		}
+		return false;
+	}
 }
