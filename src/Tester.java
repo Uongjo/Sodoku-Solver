732d7f5f1ba15466b99fdc18692b0665ee8d9fdf
Uongjo <Uongjo@utexas.edu>
2017-12-21 00:11:17 -0600

    Personal testing on 4 x 4 Sudoku puzzle

--- src/Tester.java
+++ src/Tester.java
@@ -1,15 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Tester {
 
-	public static void main(String[] args) {
-		int[][] con = new int[9][9];
-		for(int i = 0; i < 9; i++) {
-			for(int j = 0; j < 9; j++) {
-				con[i][j] = i + 1;
-			}
-		}
-		Board b = new Board(con);
-		System.out.println(b);
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner s = new Scanner(new File("test.txt"));
+		Solver test = new Solver(s);
+		System.out.println(test.solve());
+		
 	}
+	
 
 }
